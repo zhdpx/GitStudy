fbbba9dfd4b9b209edb2b8db5ddf74a2755117cc
zhdpx <17611196328@163.com>
2019-04-10 14:04:49 +0800

    添加项目

---
+++ src/main/java/com/luoning/GitStudyApplication.java
@@ -0,0 +1,12 @@
+package com.luoning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitStudyApplication.class, args);
+	}
+}
